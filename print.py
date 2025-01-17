35638dee36736729841c2d501e9b37ce29bf328d
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-17 23:57:22 +0500

    add only file of print in python

---
+++ print.py
@@ -0,0 +1,13 @@
+# to print hello world
+print("Hello world")
+
+# also print anything else
+print('Muhammad Huzaifa')
+print('techbyhuzaifa')
+
+# print multiple statement in one line
+print('heloo my name is huzaifa ', 'and i am from swabi')
+
+# also print numbers and its calculation
+print('the sum of 10 and 12 is ',10+12)
+print('this is first line and'+' this is second line')
