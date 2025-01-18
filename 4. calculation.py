75104ba7a644b8f3dded3ab0b52c11111e047d8e
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-18 17:07:43 +0500

    make some update in calculation file

--- 4. calculation.py
+++ 4. calculation.py
@@ -1,3 +1,6 @@
+# Calculation
+
+# Addition
 print('Addition------')
 num1 = 12;
 num2 = 13;
@@ -7,12 +10,13 @@ sum = num1 + num2;
 print('the sum of num1 and num2 is : ',sum)
 print('---------------')
 
+# Subtraction
 print('Subtraction------')
 diff = num1 - num2
 print('the difference between num1 and num2 is : ',diff)
 print('---------------')
 
-
+# Multiplication
 print('Multiplication------')
 num1 = 10
 num2 = 20
@@ -22,6 +26,7 @@ print('the value of num2 is now :', num2)
 print('the multiplication of num1 and num2 is : ',multi)
 print('---------------')
 
+# Division
 print('Division------')
 Division = num1/num2
 print('the division of num1 and num2 is : ',Division)
