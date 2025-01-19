d8c1b05e749310bc935a81e2d6fd50c3c4655c03
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-19 19:25:18 +0500

    add some more code lines to expression execution

---
+++ 5. .expression_execution.py
@@ -0,0 +1,41 @@
+# expression execution 
+
+# string and numeric value can be operate with *
+a,b = 2,3
+text = "@"
+print(a*text*b)
+
+# string and string can operate with +
+a = 5
+b = '3'
+text = '%'
+print((b+text)*a)
+
+# number to number operation 
+d = 3
+e = 2
+calculate = e**d
+# '**' means power in mathematics
+print('the value of d is : ',d)
+print('the value of e is : ',e)
+print('the result of e**d is : ',calculate)
+print('the sum of d and e is : ',d + e)
+print('the difference of d and e is : ',d - e)
+print('the multiplication of d and e is : ',d * e)
+print('the division of d and e is : ',d / e)
+print('the result of (d + e * d) : ',d+e*d)
+print('------------------------------------')
+
+# the arithematic operation of integer and float will always be float
+a = 1
+b = 2.2
+print('the result of a + b is : ',a+b)
+print('the result of a - b is : ',a-b)
+print('the result of a * b is : ',a*b)
+
+print('---------------------------------')
+# the result of division operator with two integers will be float
+a = 12
+b = 21
+print('the result of a/b is : ',a/b)
+
